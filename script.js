5762c3df5117cee4f35f01ffe6d3e95b5915e897
Tamema-TT <tamema.tama@northsouth.edu>
2022-10-26 17:14:48 +0600

    Updated

--- script.js
+++ script.js
@@ -7,48 +7,29 @@ function getInputValues(section) {
     const input = document.getElementById('input-' + section);
     const inputValue = parseFloat(input.value);
     if(inputValue < 0) {
-        console.log('in neg')
         const alertNeg = document.getElementById('alert-neg');
         alertNeg.innerText = 'Type positive numbers in ' + section;
         alertNeg.style.display = 'block';
         return 'negative';
     }
-    
     else if(isNaN(inputValue) == true) {
-        console.log('in nan')
         const alertNan = document.getElementById('alert-nan');
-        alertNan.innerText = 'Type valid numbers in ' + section;
+        alertNan.innerText = 'Type valid numbers';
         alertNan.style.display = 'block';
         return 'nan';
     }
     else {
-        console.log('in num')
         return inputValue;
     }
 }
-
 
 document.getElementById('calculate').addEventListener('click', function() {
     const income = getInputValues('income');
     const food = getInputValues('food');
     const rent = getInputValues('rent');
     const clothes = getInputValues('clothes');
-    console.log(income)
-
-    // extra starts
-    if((income != ('negative' && 'nan')) && (food != ('negative' && 'nan'))){
-        console.log(income)
-        console.log('error')
-    }
-
-    if((income && food) != ('negative' && 'nan')){
-        console.log(income)
-        console.log('error')
-    }
-    // extra ends
 
     if((income != ('negative' && 'nan')) && (food != ('negative' && 'nan')) && (rent != ('negative' && 'nan')) && (clothes != ('negative' && 'nan'))){
-        console.log('if')
         const totalExpenses = document.getElementById('total-expenses');
         totalExpenses.innerText = food + rent + clothes;
         getBalance().innerText = income - totalExpenses.innerText;
